546547b7d5f93356a6a8e4f1b6c733b6da453e2f
artemnetsov745 <73922920+artemnetsov745@users.noreply.github.com>
2021-12-02 04:28:37 +0300

    Update urls.py

--- CRUD_system/person_manager/urls.py
+++ CRUD_system/person_manager/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='all_people/', permanent=True)),
     path('all_people/', views.PersonListView.as_view(), name='all_people'),
     path('person/<int:pk>/', views.PersonDetailView.as_view(), name='person_detail'),
     path('delete_person/<int:pk>/', views.PersonDeleteView.as_view(), name='delete_person'),
